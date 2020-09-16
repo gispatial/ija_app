01be706110597332f89e8d3fa745c28f854b9fe7
GiSpatial Technology Malaysia <react7tech@gmail.com>
2020-09-16 13:23:11 +0800

    first commit

---
+++ App.vue
@@ -0,0 +1,18 @@
+<template>
+  <view class="container">
+    <text class="text-color-primary">The Fitness App Project by Ijaa Aini</text>
+  </view>
+</template>
+
+<style>
+.container {
+  background-color: #ffcdd2;
+  align-items: center;
+  justify-content: center;
+  flex: 6;
+  font-size: ;
+}
+.text-color-primary {
+  color: teal;
+}
+</style>
